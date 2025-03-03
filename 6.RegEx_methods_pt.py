b05f074f00b563e7502d42ed802062000d8231ef
Elide <zavalavinagreelide@gmail.com>
2025-03-02 19:02:24 -0600

    Agregar ejemplos de métodos de expresiones regulares en Python - Parte 1

---
+++ 6.RegEx_methods_pt.py
@@ -0,0 +1,35 @@
+# --------------------------------------------------------------------
+# ---------- **## Regular Expressions Methods Part 1 ##** ------------
+
+import re
+
+# --------------------------------------------------------------------
+# *-*-*-*-*-*- Example 14 ->>>> Findall() Method -*-*-*-*-*-*-*-*-*-*-
+# --------------------------------------------------------------------
+# The findall() method returns a list containing all matches.
+
+# Syntax: re.findall(pattern, string, flags=0)
+
+
+
+# *-*-*-*-*-*- Example 15 ->>>> Split() Method -*-*-*-*-*-*-*-*-*-*-
+# --------------------------------------------------------------------
+# The split() method returns a list where the string has been split at each match.
+
+# Syntax: re.split(pattern, string, maxsplit=0, flags=0)
+
+
+# *-*-*-*-*-*- Example 16 ->>>> Sub() Method -*-*-*-*-*-*-*-*-*-*-
+# --------------------------------------------------------------------
+# The sub() method replaces the matches with the text of your choice.
+
+# Syntax: re.sub(pattern, repl, string, count=0, flags=0)
+
+
+# *-*-*-*-*-*- Example 17 ->>>> Subn() Method -*-*-*-*-*-*-*-*-*-*-
+# --------------------------------------------------------------------
+# The subn() method is similar to sub() but returns a tuple containing the new string value and the number of replacements.
+
+# Syntax: re.subn(pattern, repl, string, count=0, flags=0)
+
+
