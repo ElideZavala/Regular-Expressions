dee4e6465982e6c7531476ade8f036dff3ad1143
Elide <zavalavinagreelide@gmail.com>
2025-03-30 21:17:40 -0600

    Agregar ejemplos de uso de \b y \B en expresiones regulares

--- 10.special_sequences_pt-2.py
+++ 10.special_sequences_pt-2.py
@@ -27,3 +27,59 @@ import re
 # *-*-*-*-*-*-*-*-*-* Example 28->>> \b *
 
 # *-*-*-*-* Case 1 - At the begginning of the string *-*-*-*-*
+# \b
+
+# string = "possibleim 12 54 # . hi possible \n There possible \t < ? _ > %  ^ impossible"
+
+# Buscara por la palabra possible que se encuentre al inicio de la cadena
+# match_result = re.finditer(r"\bpossible", string)
+# \b se utiliza para buscar la palabra que se encuentre al inicio de la cadena
+
+
+# # *-*-*-*-* Case 2 - At the end *-*-*-*-*
+# string = "possibleim 12 54 # . hi possible \n There possible \t < ? _ > %  ^ impossible"
+
+# match_result = re.finditer(r"\Bpossible", string)
+# # \b se utiliza para buscar la palabra que se encuentre al final de la cadena
+
+# for match in match_result:
+#     print(match)
+
+# *-*-*-*-*-*-*-*-*-* Example 29->>> \B *
+
+# *-*-*-*-* Case 1 - Not at the begginning  *-*-*-*-*
+
+# string = "heater 12 54 # . hi \n There  \t < ? _ > %  ^ unheated noheat"
+
+# match_result = re.finditer(r"\Bheat", string)
+# # \B se utiliza para buscar la palabra que no se encuentre al inicio de la cadena
+# # aqui se obtiene las palabras, unheated y noheat 
+
+# for match in match_result:
+#     print(match)
+
+
+
+# *-*-*-*-* Case 2 - Not at the end  *-*-*-*-*
+
+string = "heater 12 54 # . hi \n There  \t < ? _ > %  ^ unheated noheat"
+
+match_result = re.finditer(r"heat\B", string)
+# \B se utiliza para buscar la palabra que no se encuentre al final de la cadena
+# aqui se obtiene las palabras, heater y unheated, es decir empesara con la h, que heat no se ecuentre al final de la cadena
+
+for match in match_result:
+    print(match)
+
+'''
+<re.Match object; span=(0, 4), match='heat'>
+<re.Match object; span=(46, 50), match='heat'>
+'''
+
+
+
+
+
+
+
+
