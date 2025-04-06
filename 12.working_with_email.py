619fc36d8fc71b39835774d4837b491795803c60
Elide <zavalavinagreelide@gmail.com>
2025-04-06 17:48:09 -0600

    Se manejan patrones para hacer match con correos

--- 12.working_with_email.py
+++ 12.working_with_email.py
@@ -12,16 +12,22 @@ with open("emails.txt") as file:
     # guion -
     # El + es para que se repita una o mas veces
     pattern = r""
-    pattern = r"[a-zA-Z0-9]+@"
-    pattern = r"[a-zA-Z0-9-_]+@"
-    pattern = r"[a-zA-Z0-9-_\.]+@"
-    pattern = r"[a-zA-Z0-9-_\.]+@[a-zA-Z-]+"
+    pattern = r"[a-zA-Z0-9]+@" # obtenenis los valores que coincidan             
+    pattern = r"[a-zA-Z0-9-_]+@"   # obtenemos       
+    pattern = r"[a-zA-Z0-9-_\.]+@"  
+    pattern = r"[a-zA-Z0-9-_\.]+@[a-zA-Z-]+" # Obtenemos 
     # pattern = r"[a-zA-Z0-9-_\.]+@[a-zA-Z-]+\."
     # pattern = r"[a-zA-Z0-9-_\.]+@[a-zA-Z-]+\.com"
     pattern = r"[a-zA-Z0-9-_\.]+@[a-zA-Z-_]+\.(com|edu|io)+"
     pattern = r"[a-zA-Z0-9-_\.]+@[a-zA-Z-_]+\.[a-z]+"
+    pattern = r"([a-zA-Z0-9-_\.]+)@([a-zA-Z-_]+)\.([a-z]+)" 
 
-    matches_result = re.finditer(pattern, emails)
+
+    matches_result = re.finditer(pattern, emails) 
 
     for match in matches_result:
-        print(match)
+        # print(match)    
+        # print(match.group()) # obtenemos el valor que coincida
+        # print(match.group(1)) # Obtenemos el valor que coincida en el primer grupo
+        print(match.group(1,2,3)) # obtenemos el valor que coincida con cada grupo
+        # print(match.span()) # obtenemos la posicion de la coincidencia   
